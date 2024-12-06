4a22a80858098d39b8e4388c526bd8b876546a63
Ruben Vega Bjørkøy <41775262+RubenBjorkoy@users.noreply.github.com>
2024-12-06 11:21:40 +0100

    Delete src/spotify.js

--- src/spotify.js
+++
@@ -1,5 +0,0 @@
-var client_id = 'e718cc593f74441da1d625bfa2bb6055'; // Your client id
-var client_secret = '6d466417568846bba46a0cf9a72ee52f'; // Your secret
-var redirect_uri = 'http://localhost/'; // Your redirect uri
-
-var scopes = 'ruben.bjorkoy@gmail.com';
